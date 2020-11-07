166d551e8d9c9f7aefaae7a1d2bc5f07007b1bd6
anuragkotamraju <67843523+anuragkotamraju@users.noreply.github.com>
2020-11-06 18:15:40 -0600

    Python3

--- python_sandbox_starter/conditionals.py
+++ python_sandbox_starter/conditionals.py
@@ -1,19 +1,44 @@
 # If/ Else conditions are used to decide to do something based on something being true or false
 
-
+x=3
+y=3.0
+str1 = 'Jak'
+str2 = 'Jak'
 
 # Comparison Operators (==, !=, >, <, >=, <=) - Used to compare values
 
+""" if x>y:
+    print (f'{x} is greater than {y}')
+else:
+    print(f'{y} is greater than {x}') """
 
+""" if x>y:
+    print (f'{x} is greater than {y}')
+elif x==y:
+    print (f"{x} is equal to {y}")
+else:
+    print(f'{y} is greater than {x}') """
 
 # Logical operators (and, or, not) - Used to combine conditional statements
+# if x>3 and x<=10:
+#     print (True)
 
+# if x>2 or x<=10:
+#     print (True)
 
-
+# if not(x<2):
+#     print (True)
 
 # Membership Operators (in, not in) - Membership operators are used to test if a sequence is presented in an object
 
+# numbers = [1,2,3,4]
 
+# if y not in numbers:
+#     print ('in')
+# else:
+#     print ('Not in')
 
 
 # Identity Operators (is, is not) - Compare the objects, not if they are equal, but if they are actually the same object, with the same memory location:
+if x == y:
+    print (x is y)
--- python_sandbox_starter/loops.py
+++ python_sandbox_starter/loops.py
@@ -1,5 +1,23 @@
 # A for loop is used for iterating over a sequence (that is either a list, a tuple, a dictionary, a set, or a string).
+people = ['Jak', 'Paul','Mary']
 
+# for person in people:
+#     print(f'{person}')
+
+# for person in people:
+#     if person == 'Paul':
+#         continue
+#     print(f'Current Person: {person}')
+
+# for i in range(len(people)):
+#     print(people[i])
+
+# for i in range(11):
+#     print (i)
 
 
 # While loops execute a set of statements as long as a condition is true.
+i =0
+while i<10:
+    print(i)
+    i+=1
